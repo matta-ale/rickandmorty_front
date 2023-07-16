85f18cb9d0052703a2f99ab46cb8365cca7dad1b
Alexis Mattaruco <matta.ale@gmail.com>
2023-07-16 08:56:46 -0300

    retoques en front para mobile

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import {store} from '../src/redux/store';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 
-// const base= 'http://localhost:3001' //usar esta para trabajar local
-const base = 'https://rickandmortyback-production-75e0.up.railway.app/' //usar esta para deployar
+axios.defaults.baseURL= 'http://localhost:3001' //usar esta para trabajar local
+//axios.defaults.baseURL = 'https://rickandmortyback-production-75e0.up.railway.app/' //usar esta para deployar
 
-axios.defaults.baseURL = base //usar esta para trabajar local
 
 // ReactDOM.render(
 // <BrowserRouter>  
